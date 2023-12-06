052b5aa87be57b8c604ff903cedd09ff5056af6d
TheOne21 <84342347+TheOneTwentyOne@users.noreply.github.com>
2023-12-06 01:46:27 -0500

    Delete Python Playlist Downloader directory

--- Python Playlist Downloader/deleteMp4File.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Delete the MP4 file
-def deleteMp4File(outputDir):
-    for filename in os.listdir(outputDir):
-        if filename.endswith(".mp4"):
-            mp4File = os.path.join(outputDir, filename)
-            # Delete the mp4 file
-            os.remove(mp4File)
--- Python Playlist Downloader/replaceHashtagSymbol.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Replaces "#" with "." for any of the files in the directory.
-def replaceHashtagSymbol(outputDir):
-    for filename in os.listdir(outputDir):
-        if "#" in filename:
-            newFilename = filename.replace("#", ".")
-            os.rename(os.path.join(outputDir, filename), os.path.join(outputDir, newFilename))
